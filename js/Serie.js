2029c6b9d80f2527d6d7688c0b9d0150ce218f2e
MicaUtn <micatorales12gmail.com>
2025-05-12 21:57:38 -0300

    cambios, falta guardados y otros estilos

--- js/Serie.js
+++ js/Serie.js
@@ -1,13 +1,17 @@
 export default class Serie{
     constructor(id,url,name,language,generes,image){
-        this.id = id;
+        this.id = id,
         this.url = url,
         this.name = name;
         this.language = language; 
         this.generes = generes; 
         this.image = image;  
     }
-
+    guardarSerie() {
+        const clave = `serie${this.id}`; //aca traigo el id de mi serie
+        localStorage.setItem(clave, this.toJsonString());  //local storage solo recive strings
+        alert(`Serie "${this.name}" GUARDADOO`); 
+    }
     toJsonString() {
         return JSON.stringify({
         id: this.id,
@@ -31,6 +35,7 @@ export default class Serie{
     }
     createHtmlElement() {
         const contenedor = document.createElement('div');
+        contenedor.classList.add("tarjeta");
         contenedor.className = 'serie';
     
         const title = document.createElement('h2');
@@ -45,11 +50,23 @@ export default class Serie{
         const img = document.createElement('img');
         img.src = this.image;
         img.alt = `${this.name} imagen`;
-    
+
+        const btnGuardar = document.createElement('button');
+        btnGuardar.classList.add("boton")
+
+        btnGuardar.textContent = "guardar";
+        btnGuardar.addEventListener('click', () => {
+            this.guardarSerie();
+        });
+        img.addEventListener('click', () => {
+            window.open(this.url, '_blank');
+        });
+
         contenedor.appendChild(title);
         contenedor.appendChild(language);
         contenedor.appendChild(generes);
         contenedor.appendChild(img);
+        contenedor.appendChild(btnGuardar);
     
         return contenedor;
     }
--- js/main.js
+++ js/main.js
@@ -1,2 +1,109 @@
-import Serie from './Serie.js';
+import Serie from "./Serie.js";
 
+class Modelo {
+    Series;
+    constructor(){
+        this.Series=[];
+    }
+}
+class Control {
+    constructor(vista,modelo){
+        this.Vista = vista;
+        this.Modelo = modelo;
+        this.seriesPorPagina = 6; //creo constante p
+        this.paginaActual = 1;
+        this.URL = 'https://api.tvmaze.com/shows/';
+        this.registrarBotones();
+        this.CargaInicial();
+        this.Vista.Control = this;
+    }
+    CargaInicial(){
+        const inicio = (this.paginaActual - 1) * this.seriesPorPagina + 1;
+        const fin = inicio + this.seriesPorPagina;
+        const series = [];
+        let contador = inicio;
+
+        const cargarUnaSerie = () => {
+            if (contador < fin) {
+                fetch(this.URL + contador)
+                    .then(res => {
+                        if (!res.ok) throw new Error("Respuesta no OK");
+                        return res.json();
+                    })
+                    .then(data => {
+                        console.log("SERIEEEE:", data.name); // Verificá que llega
+                        const nuevaSerie = new Serie(
+                            data.id,
+                            data.officialSite || data.url,
+                            data.name,
+                            data.language,
+                            data.genres,
+                            data.image?.medium || "https://via.placeholder.com/210x295?text=Sin+Imagen"
+                        );
+                        series.push(nuevaSerie);
+                        console.log("SERIES "+series)
+                        contador++;
+                        cargarUnaSerie();
+                    })
+                    .catch(err => {
+                        console.error("Error al cargar serie:", err);
+                        contador++;
+                        cargarUnaSerie();
+                        });
+            } else {
+                this.Vista.mostrarSeries(series);
+            }
+        };
+        cargarUnaSerie();
+    }
+    registrarBotones(){
+        this.Vista.botones.btnSiguiente.addEventListener("click", (e) =>{
+            this.paginaActual++; 
+            this.CargaInicial();
+        })
+        this.Vista.botones.btnAnterior.addEventListener("click", () => {
+            if (this.paginaActual > 1) {
+                this.paginaActual--;
+                this.CargaInicial();
+            }
+        });
+        
+    }
+}
+
+class Vista{
+    panelIzquierda;
+    loPrincipal;
+    botones;
+    constructor(){
+        this.panelIzquierda = {
+            panelIzq : this.$("panel-izquierda"),
+            link1 : this.$("link_1"),
+            link2 : this.$("link_2"),
+        },
+        this.loPrincipal ={
+            principal : this.$("principal"),
+            series :this.$("series"),
+        },
+        this.botones={
+            btnAnterior: this.$("anterior"),
+            btnSiguiente:this.$("siguiente"),
+        }
+
+    }
+    $(id){
+        return document.getElementById(id);
+    }
+    mostrarSeries(arraySeries){
+        const contenedor = this.loPrincipal.series;
+        console.log(contenedor);
+        contenedor.innerHTML="";
+        arraySeries.forEach(serie => {
+            const div = serie.createHtmlElement();
+            contenedor.appendChild(div);
+        });
+    }
+}
+let vista = new Vista();
+let modelo = new Modelo();
+let control = new Control(vista, modelo);
